c3258049365b20a488f7a98aefd4a74d14eae0b0
A-d-1-t-y-a <adithyachalla8@gmail.com>
2026-04-03 16:49:18 +0530

    chore: remove Sentry integration and dependencies from the project

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
@@ -78,12 +77,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  sourcemaps: { disable: true },
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+export default nextConfig;
