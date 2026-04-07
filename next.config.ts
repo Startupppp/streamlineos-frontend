705e92d8aaff8dd7cb74f0872179a81726870c42
A-d-1-t-y-a <adithyachalla8@gmail.com>
2026-04-07 11:39:43 +0530

    refactor: remove custom TypeScript build configuration and update dependencies; add @langchain/openai package for enhanced functionality

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    tsconfigPath: "./tsconfig.build.json",
-  },
   productionBrowserSourceMaps: false,
   experimental: {
     serverActions: {
